ef2fec6bba36656492c134b4bdf9bb0f69401270
Andre Kuhnen <andrekuhnen@gmail.com>
2021-07-13 14:46:25 +0200

    formating

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 HADOOP_VERSION=3.3.0
 
 docker build -t spark3-glue .
-docker create  -it --name dummy  spark3-glue bash
+docker create -it --name dummy spark3-glue bash
 docker cp dummy:/opt/spark_clone/spark-3.1.2-bin-hadoop-provided-glue.tgz .
 docker rm -fv dummy
 wget https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz
